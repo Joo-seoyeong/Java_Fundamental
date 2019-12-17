e6ec43378af4c895a46ed7f06f508590142118c7
Joo-seoyeong <wntj95@naver.com>
2019-12-17 18:04:56 +0900

    2019-12-17

--- src/java_20191203/CrawlingDemo.java
+++ src/java_20191203/CrawlingDemo.java
@@ -2,6 +2,7 @@ package java_20191203;
 
 import java.io.IOException;
 
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
--- src/java_20191203/CrawlingThread.java
+++ src/java_20191203/CrawlingThread.java
@@ -1,6 +1,7 @@
 package java_20191203;
 
 import java.io.BufferedWriter;
+
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
